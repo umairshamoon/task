30f18f944e0aebef45c80338da11832fb9fab93a
umairshamoon <bobbyrule214@gmail.com>
2022-05-29 14:27:20 +0500

    created server folder

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -14,19 +14,16 @@ const Login = () => {
   //   handle submit
   const handleClick = async (e) => {
     e.preventDefault();
-    const response = await fetch(
-      'http://localhost:5000/api/auth/login',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      }
-    );
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
 
     if (!email || !password) {
       alert('Enter Email and Password');
